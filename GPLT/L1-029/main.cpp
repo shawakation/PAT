a91d3bd8a721c5efb945879b231b4176f650f6ed
haha <haha@163.com>
2020-01-29 15:58:09 +0800

    two new solutions.

---
+++ GPLT/L1-029/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	printf("%.1f\n",(n-100)*1.8f);
+	return 0;
+}
---
+++ GPLT/L1-045/main.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+using namespace std;
+int main()
+{
+	char arr[10];
+	scanf("%s",arr);
+	printf("Hello %s\n",arr);
+	return 0;
+}
